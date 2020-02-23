f090f062e378aaaa7b9798e4f358cfebbfd868f9
Unknown <jenny9993@hotmail.com>
2020-02-22 22:18:23 -0800

    Finish

--- DC1.py
+++ DC1.py
@@ -1,91 +1,144 @@
-import os
-import nltk.classify.util
+import collections, os,re
+import nltk.classify.util, nltk.metrics
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
 from nltk.corpus import stopwords
+from collections import defaultdict
+from nltk.metrics import precision,recall
+import itertools
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
 
-#test
-from nltk.tokenize import word_tokenize
-'''text = "God is Great! I won a lottery."
-filtered_list = [word for word in word_tokenize(text) if word.lower() not in stopwords.words('english')]
-print(word_tokenize(text))
-print(filtered_list)'''
-
-pathunlabled = "/Users/yanyan/PycharmProjects/DC1/unlabeled"
-pathpositive = "/Users/yanyan/PycharmProjects/DC1/pos"
-pathnegative = "/Users/yanyan/PycharmProjects/DC1/neg"
-
-def tokened_list(path):
-    input_reviews=[]
-    for f in os.listdir(path):
-        if f[-4:] == '.txt':
-            file = open(path+"/"+f, 'r')
-            input_reviews.append([word for word in word_tokenize(file.read()) if word.lower() not in stopwords.words('english')])
-    return input_reviews
-
-#print(tokened_list(pathpositive)[1])
-
-
-input_reviews=[]
-path = "/Users/yanyan/PycharmProjects/DC1/unlabeled"
-for f in os.listdir(path):
+'''
+pos_reviews = []
+neg_reviews = []
+pathpos = os.getcwd()+"/pos"
+pathneg = os.getcwd()+"/neg"
+for f in os.listdir(pathpos):
     if f[-4:] == '.txt':
-        file = open(path+"/"+f, 'r')
+        file = open(pathpos + "/" + f, 'r')
+
+        pos_reviews.append(file.read())
+
+for f in os.listdir(pathneg):
+    if f[-4:] == '.txt':
+        file = open(pathneg + "/" + f, 'r')
+        neg_reviews.append(file.read())
+
+
+'''
+# DC Medium
+def evaluate_classifier(method):
+    positive_fileid = movie_reviews.fileids('neg')
+    negative_fileid = movie_reviews.fileids('pos')
+
+    features_negative= [(method(movie_reviews.words(fileids=[f])), 'neg') for f in positive_fileid]
+    features_positive = [(method(movie_reviews.words(fileids=[f])), 'pos') for f in negative_fileid]
+#    features_negative = [(method(f), 'neg') for f in pos_reviews]
+#    features_positive = [(method(f), 'pos') for f in neg_reviews]
+
+    # Split the data into train and test (8/2)
+    threshold_factor = 0.8
+    threshold_positive = int(threshold_factor * len(features_positive))
+    threshold_negative = int(threshold_factor * len(features_negative))
+
+    trainfeats = features_positive[:threshold_positive] + features_negative[:threshold_negative]
+    testfeats = features_positive[threshold_positive:] + features_negative[threshold_negative:]
+    classifier = NaiveBayesClassifier.train(trainfeats)
+
+    #create empty set to hold the features later on
+    trainsets = collections.defaultdict(set)
+    testsets = collections.defaultdict(set)
+
+    for i, (feats, label) in enumerate(testfeats):
+        trainsets[label].add(i)
+        observed = classifier.classify(feats)
+        testsets[observed].add(i)
+
+    print('accuracy:', nltk.classify.util.accuracy(classifier, testfeats))
+    print('pos precision:', precision(trainsets['pos'], testsets['pos']))
+    print('pos recall:', recall(trainsets['pos'], testsets['pos']))
+    print('neg precision:', precision(trainsets['neg'], testsets['neg']))
+    print('neg recall:', recall(trainsets['neg'], testsets['neg']))
+    classifier.show_most_informative_features()
+    return classifier
+
+
+# Naive Baye
+def features(words):
+    return dict([(word, True) for word in words])
+
+
+
+
+#With stop words
+stopset = set(stopwords.words('english'))
+def stopword(words):
+    return dict([(word, True) for word in words if word.lower() not in stopset])
+
+
+# Using the Bigram
+def bigram(words, score_fn=BigramAssocMeasures.chi_sq, n=200):
+    bigram_finder = BigramCollocationFinder.from_words(words)
+    bigrams = bigram_finder.nbest(score_fn, n)
+    return dict([(ngram, True) for ngram in itertools.chain(words, bigrams)])
+
+classifier_naive= evaluate_classifier(features)
+classifier_stopword= evaluate_classifier(stopword)
+classifier_bigram = evaluate_classifier(bigram)
+
+'''------------------------------------------- DC Hard---------------------------------------------'''
+
+input_reviews = []
+desired_rate = []
+flags_n=[]
+flags_s=[]
+flags_b=[]
+
+#path = "/Users/yanyan/PycharmProjects/NLP-DC1/unlabeled"
+#direct = "/Users/yanyan/PycharmProjects/NLP-DC1/ratings"
+path = os.getcwd()+"/unlabeled"
+direct = os.getcwd()+"/ratings"
+
+# get the rate and convert to double
+rates = open(os.path.join(direct, "unlabeled.txt"), "r")
+review_rate = rates.readlines()
+for rate in review_rate:
+    desired_rate.append(float(rate.split()[1]))
+rates.close()
+
+
+# sort the filename with both alphabet and numbers
+def natural_key(string_):
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+
+# raise a flag
+def raise_flag(probdist_sentiment,desired_rate,flags):
+    if (probdist_sentiment == "pos" and desired_rate < 2.5) or (
+        probdist_sentiment == "neg" and desired_rate > 2.5):
+        flags.append(review)
+
+for f in sorted(os.listdir(path),key=natural_key):
+    if f[-4:] == '.txt':
+        file = open(path + "/" + f, 'r')
         input_reviews.append(file.read())
 
-def features(list):
-    #filtered_list = [word for word in list if word.lower() not in stopwords.words('english')]
-    return dict([(word, True) for word in list])
 
-if __name__=='__main__':
-   # Load positive and negative reviews
-   positive_fileid = movie_reviews.fileids('pos')
-   negative_fileid = movie_reviews.fileids('neg')
-'''
-#tokenized the reviews
-positive_tokened = [movie_reviews.words(fileids=[f])for f in positive_fileid]
-negative_tokened = [movie_reviews.words(fileids=[f])for f in negative_fileid]
+for x in range(0, len(input_reviews)):
+    review = input_reviews[x]
+    rate = desired_rate[x]
+    probdist_naive = classifier_naive.prob_classify(features(review.split())).max()
+    probdist_stopword = classifier_stopword.prob_classify(stopword(review.split())).max()
+    probdist_bigram = classifier_bigram.prob_classify(bigram(review.split())).max()
+    raise_flag(probdist_naive, rate,flags_n)
+    raise_flag(probdist_stopword, rate,flags_s)
+    raise_flag(probdist_bigram , rate,flags_b)
+    #print("\nReview:", review)
+    #print("Predicted sentiment: ", probdist_naive)
+    #print(desired_rate[x])
+    #else:
+    #    print("good")
 
-#filterout the stopwords
-filtered_words_pos = [word for word in positive_tokened if word not in stopwords.words('english')]
-filtered_words_neg = [word for word in negative_tokened if word not in stopwords.words('english')]
-
-#stop_words = list(get_stop_words('en'))         #About 900 stopwords
-#nltk_words = list(stopwords.words('english')) #About 150 stopwords
-#stop_words.extend(nltk_words)
-#output = [w for w in word_list if not w in stop_words]
-features_positive = [(features(f), 'Positive') for f in filtered_words_pos]
-features_negative = [(features(f), 'Negative') for f in filtered_words_neg]
-'''
-
-features_positive = [(features(f), 'Positive') for f in tokened_list(pathpositive)]
-features_negative = [(features(f), 'Negative') for f in tokened_list(pathnegative)]
-
-
-# Split the data into train and test (8/2)
-threshold_factor = 0.8
-threshold_positive = int(threshold_factor * len(features_positive))
-threshold_negative = int(threshold_factor * len(features_negative))
-
-features_train = features_positive[:threshold_positive]+features_negative[:threshold_negative]
-features_test = features_positive[threshold_positive:]+features_negative[threshold_negative:]
-
-#print("Number of training datapoints: ", len(features_train))
-#print("Number of test datapoints: ", len(features_test))
-
-#print(stopwords.words('english'))
-#print(positive_fileid [1])
-#print(filtered_words_pos[1])
-
-classifier = NaiveBayesClassifier.train(features_train)
-print("Accuracy of the classifier: ", nltk.classify.util.accuracy(classifier, features_test))
-
-
-print("Predictions: ")
-
-for review in input_reviews[:10]:
-    print("\nReview:", review)
-    probdist = classifier.prob_classify(features(review.split()))
-    pred_sentiment = probdist.max()
-    print("Predicted sentiment: ", pred_sentiment)
-    print("Probability: ", round(probdist.prob(pred_sentiment), 2))
+print("Inconsistency detected by Naive Baye's Classifier:" +len(flags_n)/ len(input_reviews))
+print("Inconsistency detected by Naive Baye's Classifier with Stopwords:" + len(flags_s)/len(input_reviews))
+print("Inconsistency detected by Naive Baye's Classifier with Bigram Collocation:" + len(flags_b)/len(input_reviews))
